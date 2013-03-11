4cda60ad8d3864dd75a4817a0de020b5f5030420
Laracuet <Laracuet@gmail.com>
2013-03-11 18:51:33 -0400

    changed file extensions

--- Continuum Concept.php
+++ Continuum Concept.php
@@ -13,16 +13,16 @@
 		</div>
 		<img src="Continuum Logo.gif" alt="Continuum Managed Services" id="logo"/>
 		<div id="topNav">
-		<form method="link" action="ViewRecords.html">
+		<form method="link" action="ViewRecords.php">
 				<input type="submit" value="View Records" class="topNavButton" id="viewButton">
 			</form>
-			<form method="link" action="CreateRecord.html">
+			<form method="link" action="CreateRecord.php">
 				<input type="submit" value="Create Record" class="topNavButton" id="createButton">
 			</form>
-			<form method="link" action="UpdateRecord.html">
+			<form method="link" action="UpdateRecord.php">
 				<input type="submit" value="Update Record" class="topNavButton" id="updateButton">
 			</form>
-			<form method="link" action="DeleteRecord.html">
+			<form method="link" action="DeleteRecord.php">
 				<input type="submit" value="Delete Record" class="topNavButton" id="deleteButton">
 			</form>
 		</div> 
