fd5ffaa7734155455b8ad117868682b91c065394
Laracuet <Laracuet@gmail.com>
2013-03-12 11:41:51 -0400

    Revert "Merge pull request #1 from bmwirth09/master"
    This reverts commit aca58b2bf56b87dbe5e5cb6dad59ff1f190b4b89, reversing
    changes made to 4cda60ad8d3864dd75a4817a0de020b5f5030420.

--- code_igniter/application/controllers/view.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class View extends MY_Controller {
-
-	public function index()
-	{
-		$this->makePage('ViewRecords');
-	}
-}
