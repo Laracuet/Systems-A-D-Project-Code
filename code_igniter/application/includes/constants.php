aaad0084912998078c2ed9627829133354b4e588
Brandon <bmwirth09@gmail.com>
2013-03-13 15:55:24 -0400

    includes and constants

---
+++ code_igniter/application/includes/constants.php
@@ -0,0 +1,6 @@
+<?php
+define("THEME_DIR", "/theme/");
+define("CSS_DIR", THEME_DIR . "css/");
+define("IMG_DIR", THEME_DIR . 'img/');
+define("JS_DIR", THEME_DIR . 'js/');
+define('BOOTSTRAP_DIR', THEME_DIR . 'bootstrap/');
